6652f1e8cc88c91d97940300f6a8e060698a2f4b
Prashant Shrestha <intern0t@users.noreply.github.com>
2018-08-18 19:32:08 -0400

    Added shortened URL feature using md5(unix_timestamp).

--- Backend/index.js
+++ Backend/index.js
@@ -1,26 +1,32 @@
-/** 
+/**
  *  Copyright (c) 2018, Prashant Shrestha
  *  Document    : index.js
  *  Date        : 2018-04-15
  *  Developer   : Prashant Shrestha
  */
 
-var Express = require('express')
-    , app = Express()
-    , cors = require('cors')
-    , request = require('request-promise')
-    , Promise = require('bluebird')
-    , bodyParser = require('body-parser')
-    , fs = require('fs')
-    , lowdb = require('lowdb')
-    , FileSync = require('lowdb/adapters/FileSync')
-    , adapter = new FileSync('storage/oblique.json')
-    , database = lowdb(adapter)
-    , morgan = require('morgan')
-    , PORT = process.env.PORT || 1338;
+var Express = require("express"),
+    app = Express(),
+    cors = require("cors"),
+    request = require("request-promise"),
+    Promise = require("bluebird"),
+    bodyParser = require("body-parser"),
+    fs = require("fs"),
+    lowdb = require("lowdb"),
+    FileSync = require("lowdb/adapters/FileSync"),
+    adapter = new FileSync("storage/oblique.json"),
+    database = lowdb(adapter),
+    morgan = require("morgan"),
+    md5 = require("md5"),
+    PORT = process.env.PORT || 1338;
 
 // Enabling CORS in our Express module.
-var allowedDomains = ["http://o.prashant.me", "https://o.prashant.me"];
+var allowedDomains = [
+    "127.0.0.1",
+    "localhost",
+    "http://o.prashant.me",
+    "https://o.prashant.me"
+];
 var corsOptions = {
     origin: (origin, callback) => {
         if (allowedDomains.indexOf(origin) !== -1) {
@@ -29,13 +35,13 @@ var corsOptions = {
             callback("Not allowed by CORS.", false);
         }
     },
-    methods: ['GET', 'POST'],
-    optionsSuccessStatus: 200,
+    methods: ["GET", "POST"],
+    optionsSuccessStatus: 200
 };
 
 // Using CORS & Morgan
 app.use(cors(corsOptions));
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // Enabling POST data transfer.
 app.use(bodyParser.json());
@@ -59,54 +65,176 @@ database.defaults({ links: [], count: 0 }).write();
 var generateLink = () => {
     return Promise.try(() => {
         var apiLinks = {
-            "noun": "https://nlp.fi.muni.cz/projekty/random_word/run.cgi?language_selection=en&word_selection=nouns&model_selection=norm&length_selection=10&probability_selection=true",
-            "adjective": "https://nlp.fi.muni.cz/projekty/random_word/run.cgi?language_selection=en&word_selection=adjecs&model_selection=norm&length_selection=10&probability_selection=true"
+            noun:
+                "https://nlp.fi.muni.cz/projekty/random_word/run.cgi?language_selection=en&word_selection=nouns&model_selection=norm&length_selection=10&probability_selection=true",
+            adjective:
+                "https://nlp.fi.muni.cz/projekty/random_word/run.cgi?language_selection=en&word_selection=adjecs&model_selection=norm&length_selection=10&probability_selection=true"
         };
 
         var generatedParams = "";
 
         return request(apiLinks["adjective"])
-            .then((response) => {
+            .then(response => {
                 return response.trim();
-            }).then((withResponse) => {
-                return request(apiLinks["noun"]).then((_response) => {
+            })
+            .then(withResponse => {
+                return request(apiLinks["noun"]).then(_response => {
                     return withResponse + _response.trim();
                 });
-            }).catch((err) => {
-
-            });
+            })
+            .catch(err => {});
     });
-}
+};
+
+/**
+ * Generate via. timestamp.
+ */
+var generateTimeStamp = () => {
+    return Promise.try(() => {
+        return Math.round(new Date().getTime() / 1000);
+    })
+        .then(timeStamp => {
+            return md5(timeStamp)
+                .substr(0, 5)
+                .toUpperCase();
+        })
+        .catch(err => {
+            throw new Error({
+                error: true,
+                message: "Couldn't create a shortened md5(timestamp)."
+            });
+        });
+};
 
 /**
  * Check Duplicate Entries.
  */
-var checkDuplicates = (longLink) => {
-    return database.get('links').find({ oLink: longLink }).value();
-}
+var checkDuplicates = longLink => {
+    return database
+        .get("links")
+        .find({ oLink: longLink })
+        .value();
+};
 
 // Main Route
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     return Promise.try(() => {
         res.json({
             error: false,
             message: "Welcome to the backend of Project Oblique!",
-            description: "Project Oblique uses LowDB, Express and Promises to shorten long links. Feel free to use it, it's no problem.",
-            retention: "All the shortened links are cleared at the end of the month. Reason: Just for fun and to save my storage.",
+            description:
+                "Project Oblique uses LowDB, Express and Promises to shorten long links. Feel free to use it, it's no problem.",
+            retention:
+                "All the shortened links are cleared at the end of the month. Reason: Just for fun and to save my storage.",
             developer: "Prashant Shrestha"
         });
     });
 });
 
+/**
+ * Creating a shortened URL using md5(timestamp) [0,5]
+ */
+app.use("/short", (req, res, next) => {
+    return Promise.try(() => {
+        if (!req.body.link || !req.body.auth) {
+            return false;
+        } else {
+            return true;
+        }
+    }).then(isProvided => {
+        if (isProvided) {
+            if (req.body.auth == passKey) {
+                next();
+            } else {
+                res.json({
+                    error: true,
+                    message: "Sorry, you aren't allowed to do that."
+                });
+            }
+        } else {
+            res.json({
+                error: true,
+                message:
+                    "Missing vital information required to shorten your URL."
+            });
+        }
+    });
+});
+
+app.post("/short", (req, res) => {
+    return Promise.try(() => {
+        var dbQuery = database
+            .get("links")
+            .find({ oLink: req.body.link })
+            .value();
+
+        if (!dbQuery) {
+            return generateTimeStamp();
+        } else {
+            throw {
+                error: true,
+                message: "The link you provided already has a shortened link!",
+                shortened: domain + dbQuery.nLink
+            };
+        }
+    })
+        .then(verifiedShortCode => {
+            var newEntry = {
+                oLink: req.body.link,
+                nLink: verifiedShortCode,
+                date: Date.now()
+            };
+            return newEntry;
+        })
+        .then(parsedData => {
+            if (!parsedData) {
+                throw new Error({
+                    error: true,
+                    message: "There was a problem with parsedData section.."
+                });
+            } else {
+                var dbQuery = database
+                    .get("links")
+                    .push(parsedData)
+                    .write();
+                if (dbQuery) {
+                    dbQuery = database
+                        .get("links")
+                        .find({ oLink: req.body.link })
+                        .value();
+                    res.json({ error: false, link: domain + dbQuery.nLink });
+                    database.update("count", n => n + 1).write();
+                } else {
+                    throw new Error({
+                        error: true,
+                        message:
+                            "There was a problem with executing the write query."
+                    });
+                }
+            }
+        })
+        .catch(err => {
+            if (err.shortened) {
+                res.json({
+                    error: true,
+                    message: err.message,
+                    shortened: err.shortened
+                });
+            } else {
+                res.json({ error: true, message: err.message });
+            }
+        });
+});
+
 // Create a new entry.
-app.use('/create', (req, res, next) => {
+app.use("/create", (req, res, next) => {
     return Promise.try(() => {
         if (!req.body.auth || !req.body.link) {
             return false;
         } else {
             return true;
         }
-    }).then((isProvided) => {
+    }).then(isProvided => {
         if (isProvided) {
             // Not need to check for users because passkey is provided.
             // Simply check and verify the passkey.
@@ -115,22 +243,27 @@ app.use('/create', (req, res, next) => {
             } else {
                 res.json({
                     error: true,
-                    message: "The authorization key you provided does not match!"
+                    message:
+                        "The authorization key you provided does not match!"
                 });
             }
         } else {
             res.json({
                 error: true,
-                message: "Please provide all the required authentication details."
+                message:
+                    "Please provide all the required authentication details."
             });
         }
     });
 });
 
-app.post('/create', (req, res) => {
+app.post("/create", (req, res) => {
     return Promise.try(() => {
         // Before generating, check if an entry with same link already exists.
-        var dbQuery = database.get("links").find({ oLink: req.body.link }).value();
+        var dbQuery = database
+            .get("links")
+            .find({ oLink: req.body.link })
+            .value();
 
         if (!dbQuery) {
             var linkParam = generateLink();
@@ -146,59 +279,78 @@ app.post('/create', (req, res) => {
 
             throw new Error("Link already exists!");
         }
-    }).then((link) => {
-        var newEntry = {
-            'oLink': req.body.link,
-            'nLink': link,
-            'date': Date.now()
-        };
-        return newEntry;
-    }).then((parsedData) => {
-        if (!parsedData) {
-            res.json({
-                error: true,
-                message: "There was a problem in parsedData variable!"
-            });
-        } else {
-            var dbQuery = database.get("links").push(parsedData).write();
-
-            if (dbQuery) {
-                dbQuery = database.get("links").find({ oLink: req.body.link }).value();
-
+    })
+        .then(link => {
+            var newEntry = {
+                oLink: req.body.link,
+                nLink: link,
+                date: Date.now()
+            };
+            return newEntry;
+        })
+        .then(parsedData => {
+            if (!parsedData) {
                 res.json({
-                    error: false,
-                    link: domain + dbQuery.nLink
+                    error: true,
+                    message: "There was a problem in parsedData variable!"
                 });
-                // Update the count.
-                database.update("count", n => n + 1).write();
             } else {
-                res.json("Problem");
+                var dbQuery = database
+                    .get("links")
+                    .push(parsedData)
+                    .write();
+
+                if (dbQuery) {
+                    dbQuery = database
+                        .get("links")
+                        .find({ oLink: req.body.link })
+                        .value();
+
+                    res.json({
+                        error: false,
+                        link: domain + dbQuery.nLink
+                    });
+                    // Update the count.
+                    database.update("count", n => n + 1).write();
+                } else {
+                    res.json("Problem");
+                }
             }
-        }
-    }).catch((err) => {
-        console.log(err.message);
-    });
+        })
+        .catch(err => {
+            console.log(err.message);
+        });
 });
 
 /**
  * Search and respond.
  */
-app.use('/find', (req, res, next) => {
+app.use("/find", (req, res, next) => {
     return Promise.try(() => {
         if (!req.body.shortLink) {
-            res.json({ error: true, message: "Please provide the short link!" });
+            res.json({
+                error: true,
+                message: "Please provide the short link!"
+            });
         } else {
             next();
         }
     });
 });
 
-app.post('/find', (req, res) => {
+app.post("/find", (req, res) => {
     return Promise.try(() => {
-        var dbQuery = database.get("links").find({ nLink: req.body.shortLink }).value();
+        var dbQuery = database
+            .get("links")
+            .find({ nLink: req.body.shortLink })
+            .value();
 
         if (!dbQuery) {
-            res.json({ error: true, message: "Could not find any short links with the provided short code." });
+            res.json({
+                error: true,
+                message:
+                    "Could not find any short links with the provided short code."
+            });
         } else {
             res.json({ error: false, message: dbQuery });
         }
@@ -208,12 +360,15 @@ app.post('/find', (req, res) => {
 /**
  * Get total links in our database.
  */
-app.get('/count', (req, res) => {
+app.get("/count", (req, res) => {
     return Promise.try(() => {
         var dbQuery = database.get("count").value();
 
         if (!dbQuery) {
-            res.json({ error: true, message: "There was a problem retrieving total URL count." });
+            res.json({
+                error: true,
+                message: "There was a problem retrieving total URL count."
+            });
         } else {
             res.json({ error: false, message: dbQuery });
         }
